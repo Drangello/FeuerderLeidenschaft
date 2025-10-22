a26bda69517c8d8c8764adee9c85756fb30fc0bd
Drangello <drangello0505@gmail.com>
2025-10-22 20:07:00 +0200

    finish all struction to start with the Mechanics

--- models/character.class.js
+++ models/character.class.js
@@ -1,6 +1,7 @@
 class Character extends MovableObject{
-    y = 110;
-    width = 300;
+    y = 150;
+    width = 200;
+    height = 300;
     constructor(){
         super().loadImage('img/2_character_pepe/2_walk/W-21.png');
     }
--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,5 +1,7 @@
 class Chicken extends MovableObject{  
-    y = 300;
+    y = 350;
+    width = 100;
+    height = 100;
     constructor(){
         super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
 
